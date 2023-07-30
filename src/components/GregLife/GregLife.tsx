a21015746eafdedfaf113b44c5e047bdda87c58d
Greg <gregm31@live.com>
2023-07-30 10:39:43 +1000

    updating asset images, first section ccontent complete

--- src/components/GregLife/GregLife.tsx
+++ src/components/GregLife/GregLife.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Typography, Image, Section } from '@/components'
-import gregWalkingDownGardenPath from '@/assets/images/greg-walking-down-garden-path.webp'
+import canvaRooftop from '@/assets/images/greg-at-canva-peace-sign.jpg'
+import cuddleCats from '@/assets/images/greg-cuddling-cats-with-grin.jpg'
 import styles from './gregLife.styles.module.css'
 
 export function GregLife() {
@@ -11,13 +12,22 @@ export function GregLife() {
 			className={styles.wrapper}
 		>
 			{/* TODO: change this to something useful */}
-			<Image
-				alt="My first image"
-				src={gregWalkingDownGardenPath}
-				width={500}
-				height={500}
-			/>
-
+			<div className={styles.imageWrapper}>
+				<Image
+					alt="Greg holding his two cats Sesame and Rame and smiling excitedly"
+					src={cuddleCats}
+					width={190}
+					height={190}
+					className={styles.cuddleCats}
+				/>
+				<Image
+					alt="My first image"
+					src={canvaRooftop}
+					width={500}
+					height={500}
+					className={styles.canvaRooftop}
+				/>
+			</div>
 			<div className={styles.type}>
 				<Typography type="h1">
 					Greg life <a id="greg-life" />
@@ -25,7 +35,8 @@ export function GregLife() {
 				<Typography variant="pLead">
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas labore
 					minus porro ad. Praesentium accusamus error nam facilis explicabo
-					aliquid.
+					aliquid. Lorem, Lorem ipsum dolor sit amet consectetur adipisicing
+					elit. Delectus
 				</Typography>
 			</div>
 		</Section>
--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,6 +1,6 @@
 import NextImage, { ImageProps } from 'next/image'
 import styles from './image.styles.module.css'
 
-export function Image({ ...delegated }: ImageProps) {
-	return <NextImage {...delegated} className={styles.image} />
+export function Image({ className, ...delegated }: ImageProps) {
+	return <NextImage {...delegated} className={`${styles.image} ${className}`} />
 }
