9ececd234f0916792071467efc10e110def4974d
Greg Martin <gregm31@live.com>
2023-08-02 01:04:28 +1000

    WIP

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -14,26 +14,60 @@ const TESTIMONIALS = [
 		name: 'John Smith',
 		role: 'King of the shits',
 	},
+	{
+		img: testimonialImage,
+		imgAltText: 'Testimonial will go here',
+		leadText: 'Lorem ipsum dolor sit amet.',
+		bodyText:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum laboriosam explicabo numquam deserunt est adipisci ducimus? Maiores itaque consectetur similique!',
+		name: 'Smith John',
+		role: 'King of the shits',
+	},
+	{
+		img: testimonialImage,
+		imgAltText: 'Testimonial will go here',
+		leadText: 'Lorem ipsum dolor sit amet.',
+		bodyText:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum laboriosam explicabo numquam deserunt est adipisci ducimus? Maiores itaque consectetur similique!',
+		name: 'John Smith',
+		role: 'King of the shits',
+	},
+	{
+		img: testimonialImage,
+		imgAltText: 'Testimonial will go here',
+		leadText: 'Lorem ipsum dolor sit amet.',
+		bodyText:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum laboriosam explicabo numquam deserunt est adipisci ducimus? Maiores itaque consectetur similique!',
+		name: 'John Smith',
+		role: 'King of the shits',
+	},
 ]
 
 export function Testimonials() {
 	const [currentTestimonial, setCurrentTestimonial] = useState(0)
+
 	return (
 		<Section
 			ariaLabel="Testimonials about Greg as a person/developer"
 			className={styles.wrapper}
 		>
-			{TESTIMONIALS.map(testimonial => (
+			<div className={styles.blockQuoteWrapper}>
 				<Testimonial
-					key={testimonial.name}
-					img={testimonial.img}
-					imgAltText={testimonial.imgAltText}
-					leadText={testimonial.leadText}
-					bodyText={testimonial.bodyText}
-					name={testimonial.name}
-					role={testimonial.role}
+					key={TESTIMONIALS[currentTestimonial].name}
+					img={TESTIMONIALS[currentTestimonial].img}
+					imgAltText={TESTIMONIALS[currentTestimonial].imgAltText}
+					leadText={TESTIMONIALS[currentTestimonial].leadText}
+					bodyText={TESTIMONIALS[currentTestimonial].bodyText}
+					name={TESTIMONIALS[currentTestimonial].name}
+					role={TESTIMONIALS[currentTestimonial].role}
 				/>
-			))}
+			</div>
+			<div className={styles.buttonsWrapper}>
+				<button className={styles.button} />
+				<button className={styles.button} />
+				<button className={styles.button} />
+				<button className={styles.button} />
+			</div>
 		</Section>
 	)
 }
