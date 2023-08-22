4b816b82b0689c539808bd1e82ef9ecdd938973b
Greg Martin <gregm31@live.com>
2023-08-22 16:33:21 +1000

    Greg's values now responsive HUZZUH

--- src/components/GregsValues/GregsValues.tsx
+++ src/components/GregsValues/GregsValues.tsx
@@ -5,11 +5,16 @@ export function GregsValues() {
 	return (
 		<Section bgColor="red" ariaLabel="My values" className={styles.wrapper}>
 			<div className={styles.textWrapper}>
-				<Typography type="h2" color="white" variant="programHeader">
+				<Typography
+					type="h2"
+					color="white"
+					variant="programHeader"
+					className={styles.header}
+				>
 					<a id="gregs-values" className="anchor" />
 					Greg&apos;s values
 				</Typography>
-				<Typography color="white">
+				<Typography color="white" className={styles.body}>
 					I&apos;m a bit torn about what to throw down here, between writing
 					down what my values are, or explaining how they align with
 					Canva&apos;s values. I feel as though they align so well however,
