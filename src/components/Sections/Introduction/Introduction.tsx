5671ffe751a83bacb098745851a970d441cd4c08
Greg Martin <gregm31@live.com>
2023-09-07 08:02:06 +1000

    Main pf introduction work, quick canva testimonial fix

--- src/components/Sections/Introduction/Introduction.tsx
+++ src/components/Sections/Introduction/Introduction.tsx
@@ -5,31 +5,30 @@ import styles from './introduction.styles.module.css'
 
 export function Introduction() {
 	return (
-		<Section
-			ariaLabel="Introduction to Greg's portfolio"
-			className={styles.wrapper}
-		>
-			<SocialLinks size={20} color="secondary" />
-			<Typography color="primary" weight="bold" type="h1" size="six-xl">
-				Greg&apos;s Place
-			</Typography>
-			<Typography
-				weight="light"
-				type="span"
-				size="two-xl"
-				className={styles.tagLine}
-			>
-				A Full-Stack Engineer, standing in front of JavaScript, asking it to
-				love him.
-			</Typography>
-			<div className={styles.imageWrapper}>
-				<Image
-					src={meWide}
-					alt="Greg smiling and looking left with a funky Hawaiian shirt on."
-					fill
-					sizes="100vw"
-					placeholder="blur"
-				/>
+		<Section ariaLabel="Introduction to Greg's portfolio">
+			<div className={styles.wrapper}>
+				<SocialLinks size={20} color="secondary" />
+				<Typography color="primary" weight="bold" type="h1" size="six-xl">
+					Greg&apos;s Place
+				</Typography>
+				<Typography
+					weight="light"
+					type="span"
+					size="two-xl"
+					className={styles.tagLine}
+				>
+					A Full-Stack Engineer, standing in front of JavaScript, asking it to
+					love him.
+				</Typography>
+				<div className={styles.imageWrapper}>
+					<Image
+						src={meWide}
+						alt="Greg smiling and looking left with a funky Hawaiian shirt on."
+						fill
+						sizes="100vw"
+						placeholder="blur"
+					/>
+				</div>
 			</div>
 		</Section>
 	)
