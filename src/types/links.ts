f5519b3fd149bcfde42e682e3be6d403b5015639
Greg Martin <gregm31@live.com>
2023-09-07 15:37:53 +1000

    removed incorrect types

--- src/types/links.ts
+++ src/types/links.ts
@@ -1,11 +1,10 @@
-import { IconProps } from '@radix-ui/react-icons/dist/types'
 import { ForwardRefExoticComponent, RefAttributes } from 'react'
 
 export type SocialLink = {
 	key: string
 	linkTooltipText: string
 	href: string
-	icon: ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>
+	icon: SVGSVGElement
 	screenReader: string
 	attributes?: {
 		download: string
