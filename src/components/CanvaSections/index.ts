8ab7acd2906a4f616e9296475c8f5ad4fc09aa82
Greg Martin <gregm31@live.com>
2023-09-09 09:07:58 +1000

    fix export

--- src/components/CanvaSections/index.ts
+++ src/components/CanvaSections/index.ts
@@ -1,6 +1,7 @@
 export * from './BeHired'
 export * from './GregLife'
 export * from './GregLifeMural'
+export * from './GregsValues'
 export * from './MyContribution'
 export * from './MyJobs'
 export * from './Testimonials'
