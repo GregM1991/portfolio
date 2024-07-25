8274827a5151261c467c74cc806c0f252e339510
Greg <gregm31@live.com>
2024-07-26 05:16:39 +1000

    Might remove work history all-together

--- src/app/(homeGroup)/(projects)/work-history/workHistoryContent.ts
+++ src/app/(homeGroup)/(projects)/work-history/workHistoryContent.ts
@@ -1,3 +1,13 @@
 export const summary = `Throughout my journey as a Full Stack Engineer, I have been widely interested in the entire environment of Software Engineering, but have been most engaged by the front end. I have a passion for clean, scalable code and have proven skills in JavaScript & Typescript and supporting frameworks/libraries that solve creative problems. I'm an agile thinker and strong communicator, motivated to grow my skill-set through new challenging projects and prove my value in any endeavor I take on. My previous 
 career as a Graphic Designer has given me a strong ability to talk design language 
 as well as communicate with Product teams and align on the best possible user experience outcomes.`;
+
+export const responsibilities = [
+  `My main responsibility includes planning, mapping and execution of new and updated features. Working in 6 week sprints, I take a pitch given to me and decide on requirements for implementation and take it all the way through to execution.`,
+  `Moved our AzureB2B based authentication to Auth0, creating new APIs and business logic to account for secure communication to and from Auth0 as well as spinning up new architecture using Terraform and ensuring a seamless migration of customers (SSO included).`,
+  `Carried out a SPIKE on emerging technology, Okta Fine-Grained Access Control, to understand if this new tech would be viable for eQ8 taking into consideration business needs and time constraints.`,
+  `Rebuilding and refactoring legacy c# razor pages into React (Typescript) using relevant industry standard libraries and packages.`,
+  `Splitting out tightly-coupled services from our Monolith repo to be in line with the service-based architecture we are transitioning to. Writing performant/scalable APIs using ASP.NET in c# to carry out the business logic side of our application.`,
+  `During this time I was one of two front-end engineers. I helped flesh out best practices for our front end and am heavily relied upon to be a gateway for front-end code to pass through in order to be approved for production.`,
+  `Mentoring the more junior members of the team to help support them and bring them to a level of proficiency needed for modern web standards.`
+];
