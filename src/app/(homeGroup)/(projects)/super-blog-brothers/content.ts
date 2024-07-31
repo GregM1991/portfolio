69bc20c767f442b48430db0d40d850c8b6f9550e
Greg <gregm31@live.com>
2024-08-01 06:26:50 +1000

    Adding content for Super Blog Brothers

---
+++ src/app/(homeGroup)/(projects)/super-blog-brothers/content.ts
@@ -0,0 +1,6 @@
+import SuperBlogBrothersSplash from "@/assets/images/super-blog-brothers-splash.png";
+
+const imageAltTxt = `Super Blog Brothers written on a dark background with floating geometry`;
+const copy = `Super Blog Brothers was a fun build, Andy and George are two great blokes, who came to me with a largely open brief. I talked them into some 3d assets (using Threlte JS) to help them with personlisation and they loved that, bringing this feel of games to a website about games. I got to build the site using my favourite framework SvelteKit, so the DX was really nice for me. I also opted for a CMS, Prismic, so that Andy and George could take control of the content side and add/remove posts as they wanted.`;
+
+export { SuperBlogBrothersSplash, copy, imageAltTxt };
