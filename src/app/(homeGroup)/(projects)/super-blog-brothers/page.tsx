236a075c4e26a32ecc2a6ce8e64a4a583ae2bac9
Greg <gregm31@live.com>
2024-08-01 06:26:50 +1000

    Adding content for Super Blog Brothers

--- src/app/(homeGroup)/(projects)/super-blog-brothers/page.tsx
+++ src/app/(homeGroup)/(projects)/super-blog-brothers/page.tsx
@@ -1,12 +1,15 @@
 import { LeadContentBlock } from "@/components";
 import styles from "@/app/(homeGroup)/project.styles.module.css";
+import { copy, imageAltTxt, SuperBlogBrothersSplash } from "./content";
 
 export default function SuperBlogBrothersProject() {
   return (
     <main className={styles.wrapper}>
       <LeadContentBlock
         heading="Super Blog Brothers"
-        body={[{ key: "1", copy: "WIP" }]}
+        body={[{ key: "1", copy }]}
+        imageSrc={SuperBlogBrothersSplash}
+        imageAlt={imageAltTxt}
       />
     </main>
   );
