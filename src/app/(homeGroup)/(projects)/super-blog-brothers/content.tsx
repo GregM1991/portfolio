5c958284b7b3d25d2bc16d3cdf54b258ab8db694
Greg <gregm31@live.com>
2024-08-07 05:32:38 +1000

    Added tech + goals section, small edit to copy

--- src/app/(homeGroup)/(projects)/super-blog-brothers/content.tsx
+++ src/app/(homeGroup)/(projects)/super-blog-brothers/content.tsx
@@ -1,9 +1,20 @@
-import SuperBlogBrothersSplash from "@/assets/images/super-blog-brothers-splash.png";
+import superBlogBrothersSplash from "@/assets/images/super-blog-brothers/super-blog-brothers-splash.png";
+import animatedBlocks from "@/assets/images/super-blog-brothers/blocks.png";
 import { SocialLink } from "@/types/links";
 import { FaArrowUpRightFromSquare, FaGithub } from "react-icons/fa6";
 
 const imageAltTxt = `Super Blog Brothers written on a dark background with floating geometry`;
-const copy = `Super Blog Brothers was a fun build, Andy and George are two great blokes, who came to me with a largely open brief. I talked them into some 3d assets (using Threlte JS) to help them with personlisation and they loved that, bringing this feel of games to a website about games. I got to build the site using my favourite framework SvelteKit, so the DX was really nice for me. I also opted for a CMS, Prismic, so that Andy and George could take control of the content side and add/remove posts as they wanted.`;
+const openingBody = [
+  {
+    key: "2",
+    copy: `Super Blog Brothers was a fun build, I met Andy and George through a friend of mine, and we hit it off due to our love of games. They told me about their plan to start a video game blog and the conversation flowed on to a site I could build for them.`
+  },
+  {
+    key: "2",
+    copy: `The brief they gave me was largely open, having been through a Bachelor of Comms design they trusted me with the look and feel. I talked them into some 3d assets (using Threlte JS) to help them with personlisation and they loved that, bringing this feel of game assets to a website about games. I got to build the site using my favourite framework SvelteKit, so the DX was really nice for me. I opted for a CMS, Prismic, so that Andy and George could take control of the content side and add/remove posts as they wanted.`
+  }
+];
+
 const links: SocialLink[] = [
   {
     key: "1",
@@ -20,16 +31,40 @@ const links: SocialLink[] = [
     screenReader: "A link to the Super Blog Brothers Github repository"
   }
 ];
-const techStack = ["SvelteKit", "Threelte JS", "Prismic", "Typescript"];
+
+const techStack = [
+  "SvelteKit",
+  "Threelte JS",
+  "Prismic",
+  "Typescript",
+  "GSAP",
+  "Tailwind"
+];
 const techSectionAriaLabel =
   "The tech stack used in the Super Blog Brothers project";
 
+const purposeAndGoals = {
+  heading: "Purpose and Goals",
+  body: [
+    {
+      key: "1",
+      copy: `My endgame for the Super Blog Brothers was to create a nice modern looking website that had a sense of fun about it. I wanted to replicate the kind of interest and wonder you get from playing a great video game, so I naturally was drawn towards animations and 3d.`
+    },
+    {
+      key: "2",
+      copy: `The site also needed to be easily updated and editable, so that the Brothers could add/update/delete their blog posts at will. For this I needed a CMS, and I've had some small experience with Prismic, so I went with this. I remember there being a bit of a learning curve with the old Prismic, but given the Brothers would just be using it as a content creation for blog posts, and their background in games development, I didn't think it too much of an issue.`
+    }
+  ],
+  sectionAriaLabel: "Purpose and Goals of the Super Blog Brothers project"
+};
+
 const content = {
-  SuperBlogBrothersSplash,
-  copy,
+  images: { superBlogBrothersSplash, animatedBlocks },
+  openingBody,
   imageAltTxt,
   links,
   techStack,
-  techSectionAriaLabel
+  techSectionAriaLabel,
+  purposeAndGoals
 };
 export default content;
--- src/app/(homeGroup)/(projects)/super-blog-brothers/page.tsx
+++ src/app/(homeGroup)/(projects)/super-blog-brothers/page.tsx
@@ -1,22 +1,30 @@
 "use client";
-import { LeadContentBlock, TechContentBlock } from "@/components";
+import {
+  GenericContentBlock,
+  ImageCaptionCard,
+  LeadContentBlock,
+  TechContentBlock,
+  Typography
+} from "@/components";
 import projectStyles from "@/app/(homeGroup)/project.styles.module.css";
-import styles from "./superBlogBrothers.styles.module.css";
+import superBlogBrothersStyles from "./superBlogBrothers.styles.module.css";
+
 import content from "./content";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function SuperBlogBrothersProject() {
   return (
     <main className={projectStyles.wrapper}>
       <LeadContentBlock
         heading="Super Blog Brothers"
-        body={[{ key: "1", copy: content.copy }]}
+        body={content.openingBody}
         headingProps={{ size: "2xl" }}
         links={content.links}
       />
-      <div className={styles.imageWrapper}>
+      <div className={superBlogBrothersStyles.imageWrapper}>
         <Image
-          src={content.SuperBlogBrothersSplash}
+          src={content.images.superBlogBrothersSplash}
           alt={content.imageAltTxt}
           sizes="100vw"
           placeholder="blur"
@@ -27,6 +35,46 @@ export default function SuperBlogBrothersProject() {
         sectionAriaLabel={content.techSectionAriaLabel}
         techStack={content.techStack}
       />
+      <GenericContentBlock
+        heading={content.purposeAndGoals.heading}
+        headingProps={{ type: "h2", size: "xl" }}
+        body={content.purposeAndGoals.body}
+        sectionAriaLabel={content.purposeAndGoals.sectionAriaLabel}
+      />
+      <div className={projectStyles.screensGrid}>
+        <ImageCaptionCard
+          imageSrc={content.images.animatedBlocks}
+          imageAlt="A screenshot of the whole of the home portfolio page"
+          caption={
+            <Typography size="xs">
+              These blocks give a fun sense of play to the splash screen and are
+              interactive.{" "}
+              <Link href="https://video-game-blog-6zt3lzda0-gregm1991s-projects.vercel.app/">
+                Check them out here
+              </Link>
+              , or check out the code{" "}
+              <Link href="https://github.com/GregM1991/video-game-blog/blob/main/src/lib/slices/Hero/index.svelte#L72">
+                here
+              </Link>
+            </Typography>
+          }
+          isScreenCap
+        />
+        <ImageCaptionCard
+          imageSrc={content.images.superBlogBrothersSplash}
+          imageAlt="A screenshot of the whole of the home portfolio page"
+          caption="A carousel component to showcase screens for my projects."
+          isScreenCap
+          linkToCode="https://github.com/GregM1991/portfolio/blob/main/src/components/Carousel/Carousel.tsx"
+        />
+        <ImageCaptionCard
+          imageSrc={content.images.superBlogBrothersSplash}
+          imageAlt="A screenshot of the whole of the home portfolio page"
+          caption="I had built this testimonials section for the canva page, but it was too good not to use in my regular portfolio."
+          isScreenCap
+          linkToCode="https://github.com/GregM1991/portfolio/blob/main/src/components/NewTestimonials/NewTestimonials.tsx"
+        />
+      </div>
     </main>
   );
 }
